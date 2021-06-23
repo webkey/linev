162e03829a65164b2a2285f68fb96148d16e5fa3
s.ilchenko <s.ilchenko.imedia@gmail.com>
2021-06-24 02:39:47 +0300

    Map with contacts transform to slider on devices

--- app/blocks/global/global.js
+++ app/blocks/global/global.js
@@ -11,6 +11,7 @@ app.global = {
       });
       self.hoverEvent();
     }
+    self.runSlider();
   },
   run($el, $container) {
     const self = this;
@@ -34,5 +35,65 @@ app.global = {
         $(this).switchClass('add');
       }
     });
+  },
+  runSlider() {
+    const self = this;
+    const $slider = $('.js-global-info-slider');
+
+    if ($slider.length) {
+      const $prevEl = $slider.find('.js-global-info-slider__button_prev');
+      const $nextEl = $slider.find('.js-global-info-slider__button_next');
+
+      const sliderInstance = new Swiper($slider.find('.swiper-container'), {
+        init: false,
+        // loop: true, // not work correctly along with "watchOverflow: true"
+        freeMode: true,
+        slidesPerView: self.slidesPerView,
+        slidesPerGroup: self.slidesPerView,
+        spaceBetween: 0,
+        watchSlidesVisibility: true,
+        noSwipingSelector: 'label',
+        watchOverflow: true,
+
+        navigation: {
+          prevEl: $prevEl,
+          nextEl: $nextEl,
+        },
+
+        breakpoints: {
+          1365: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          1199: {
+            slidesPerView: 3.1,
+            slidesPerGroup: 3,
+          },
+          767: {
+            slidesPerView: 2.1,
+            slidesPerGroup: 2,
+          },
+          575: {
+            slidesPerView: 1.1,
+            slidesPerGroup: 1,
+          },
+        },
+
+        on: {
+          slideChangeTransitionEnd() {
+            const $img = $(sliderInstance.visibleSlides).find('.js-lazy-load');
+            if ($img.filter('.loaded').length !== $img.length && app.lazyLoadGlobalInstance) {
+              app.lazyLoadGlobalInstance.update();
+            }
+          },
+        },
+      });
+
+      sliderInstance.on('init', () => {
+        $slider.addClass('is-loaded');
+      });
+
+      sliderInstance.init();
+    }
   },
 };
--- app/blocks/offices/offices.js
+++ app/blocks/offices/offices.js
@@ -3,6 +3,7 @@ app.offices = {
   container: '.js-offices',
   balloon: '.js-offices-balloon',
   panel: '.js-offices-panel',
+  slidesPerView: 5,
   currentId: null,
   init() {
     const self = this;
@@ -13,21 +14,22 @@ app.offices = {
     if ($control.length) {
       self.run();
     }
+    self.runSlider();
   },
   run() {
     const self = this;
 
-    $(self.balloon).on('click', function (e) {
-      e.preventDefault();
-
-      if (window.innerWidth <= 992) {
-        if (!$(this).is(':animated')) {
-          self.$page.stop().animate({
-            scrollTop: $(self.currentId).offset().top - $('header').innerHeight() - 20,
-          }, 300);
-        }
-      }
-    });
+    // $(self.balloon).on('click', function (e) {
+    //   e.preventDefault();
+    //
+    //   if (window.innerWidth <= 992) {
+    //     if (!$(this).is(':animated')) {
+    //       self.$page.stop().animate({
+    //         scrollTop: $(self.currentId).offset().top - $('header').innerHeight() - 20,
+    //       }, 300);
+    //     }
+    //   }
+    // });
 
     $(self.balloon).on('mouseenter', function () {
       self.currentId = $(this).attr('href');
@@ -47,5 +49,65 @@ app.offices = {
   },
   changeSelect() {
     app.contactsForm.chooseOption($('#contacts-office'), this.currentId.substr(1));
+  },
+  runSlider() {
+    const self = this;
+    const $slider = $('.js-offices-info-slider');
+
+    if ($slider.length) {
+      const $prevEl = $slider.find('.js-offices-info-slider__button_prev');
+      const $nextEl = $slider.find('.js-offices-info-slider__button_next');
+
+      const sliderInstance = new Swiper($slider.find('.swiper-container'), {
+        init: false,
+        // loop: true, // not work correctly along with "watchOverflow: true"
+        freeMode: true,
+        slidesPerView: self.slidesPerView,
+        slidesPerGroup: self.slidesPerView,
+        spaceBetween: 0,
+        watchSlidesVisibility: true,
+        noSwipingSelector: 'label',
+        watchOverflow: true,
+
+        navigation: {
+          prevEl: $prevEl,
+          nextEl: $nextEl,
+        },
+
+        breakpoints: {
+          1365: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          1199: {
+            slidesPerView: 3.1,
+            slidesPerGroup: 3,
+          },
+          767: {
+            slidesPerView: 2.1,
+            slidesPerGroup: 2,
+          },
+          575: {
+            slidesPerView: 1.1,
+            slidesPerGroup: 1,
+          },
+        },
+
+        on: {
+          slideChangeTransitionEnd() {
+            const $img = $(sliderInstance.visibleSlides).find('.js-lazy-load');
+            if ($img.filter('.loaded').length !== $img.length && app.lazyLoadGlobalInstance) {
+              app.lazyLoadGlobalInstance.update();
+            }
+          },
+        },
+      });
+
+      sliderInstance.on('init', () => {
+        $slider.addClass('is-loaded');
+      });
+
+      sliderInstance.init();
+    }
   },
 };
